048de7ab5ca197d7642f2e12752bd978c710da82
sri-maddineni <121371216+sri-maddineni@users.noreply.github.com>
2023-12-07 19:48:29 +0530

    placements added

--- js/placements/y20.js
+++ js/placements/y20.js
@@ -1,73 +1,12 @@
-function mainer(){
-
-
-  var card=document.getElementById("cardContainer");
-  var tit=document.getElementById("deptheading");
-  tit.innerHTML="Y20 Placements"
-  card.innerHTML='';
-  var imageslist = [
-      "https://www.snovasys.com/wp-content/uploads/2016/10/Snovasys.png",
-      "https://media.licdn.com/dms/image/C4E1BAQESTvVmxsafRQ/company-background_10000/0/1611388624887/tmachinesoftware_cover?e=2147483647&v=beta&t=rHhsMtumDkQc52rQwuKcbA1OEG9s_f8RXN-jn_4U-Jg",
-      "https://elearningindustry.com/wp-content/uploads/2019/10/52ed748d6b9b944d886a928470b5b347.jpeg",
-      "https://media.licdn.com/dms/image/D560BAQH5YHtpxAHvDA/company-logo_200_200/0/1694512590827/numetry_technologies_logo?e=2147483647&v=beta&t=cC088ox_T045191puks_Kh8YBwm6jbA3aWIp9u8Rnzc"
-  ];
-
-  
-if (card) {
-  // Container HTML
-  var containerHTML = `
-      <div class="container" style="border: solid 2px black; margin-top:40px; width:500px; height:300px; padding-bottom: 10%; position: relative; overflow: hidden;">
-          <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="true">
-              <div class="carousel-indicators">`
-
-              imageslist.forEach(function(index){
-                  containerHTML+=`<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="${index+1}" class="active" aria-current="true" aria-label="Slide 1"></button>
-                  `
-              })
-                 containerHTML+=`</div>
-              <div class="carousel-inner">`;
-
-  // Carousel Items HTML
-  var carouselItemsHTML = '';
-  imageslist.forEach(function (item, index) {
-      carouselItemsHTML += `
-          <div class="carousel-item ${index === 0 ? 'active' : ''}"  data-bs-interval="1000">
-              <img src="${item}" class="d-block" style="width:500px; height:300px; padding:10px" alt="...">
-          </div>`;
-  });
-
-  // Controls and closing tags
-  var closingHTML = `
-              </div>
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-              </button>
-          </div>
-      </div>`;
-
-  // Set the HTML content of the card element
-  card.innerHTML += containerHTML + carouselItemsHTML + closingHTML;
-} else {
-  console.error("Container element not found.");
-}
-  
-  
-}    
-
 var firebaseConfig = {
-  apiKey: "AIzaSyBSd0FFY4jYumI731SXaZ1RpAEXsrp6fog",
-  authDomain: "bec-hostels-64d05.firebaseapp.com",
-  databaseURL: "https://bec-hostels-64d05-default-rtdb.firebaseio.com",
-  projectId: "bec-hostels-64d05",
-  storageBucket: "bec-hostels-64d05.appspot.com",
-  messagingSenderId: "174951857966",
-  appId: "1:174951857966:web:abe5f329cb70d6c01ac4bf",
-  measurementId: "G-9J50LVPRKQ"
+    apiKey: "AIzaSyBSd0FFY4jYumI731SXaZ1RpAEXsrp6fog",
+    authDomain: "bec-hostels-64d05.firebaseapp.com",
+    databaseURL: "https://bec-hostels-64d05-default-rtdb.firebaseio.com",
+    projectId: "bec-hostels-64d05",
+    storageBucket: "bec-hostels-64d05.appspot.com",
+    messagingSenderId: "174951857966",
+    appId: "1:174951857966:web:abe5f329cb70d6c01ac4bf",
+    measurementId: "G-9J50LVPRKQ"
 };
 
 
@@ -77,166 +16,153 @@ firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
 
-document.getElementById("deptheading").innerHTML = "";
 
-document.getElementById("deptheading").innerHTML = " Y20 Placements";
+function mainer() {
 
 
-// Reference to Firestore
-var card=document.getElementById("cardContainer").innerHTML="";
+    var cardContainer = document.getElementById("cardContainer");
+    var tit = document.getElementById("deptheading");
 
-// Retrieve data and render cards
-db.collection("/becstuoweb/navigation/placements/")
-  .orderBy("regno", "asc")
-  .get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          renderCard(data);
-      });
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error);
-  });
+    tit.innerHTML = "Y20 Placements";
 
-try {
-  document.getElementById("deptheading").innerHTML = "";
-  document.getElementById("deptheading").innerHTML = "Y20 " + y + " Placements";
-}
-catch (error) {
-  document.getElementById("deptheading").innerHTML = " Y20 Placements";
+    cardContainer.innerHTML = '';
 
-}
+    var companylist = [];
+    var imageslist = [];
 
-// Function to render a card
-function renderCard(data) {
-  const cardContainer = document.getElementById("cardContainer");
+    db.collection(`/becstuoweb/navigation/placements/`)
+        .doc("companylist")
+        .get()
+        .then((querySnapshot) => {
 
-  // Create card element
-  const card = document.createElement("div");
-  card.className = "col-md-4 mb-4";
+            imageslist =querySnapshot.data().images;
+            companylist=querySnapshot.data().companies;
 
-  card.innerHTML = `
-  <div class="card shadow-lg col mt-4  mx-auto">
-        <div class="card-header bg-success container-fluid">
-            <p class="h4 text-warning">${data.name}</p>
+            console.log(imageslist, companylist)
+
+            generatecard(imageslist,companylist)
+        })
+        .catch((error) => {
+            console.error("Error getting documents: ", error);
+        });
+
+
+
+
+
+
+    function generatecard(imageslist, clist){
+        imageslist.forEach(function (item, index) {
+            const card = document.createElement("div");
+            card.className = "col-md-3 mb-4";
+    
+            card.innerHTML = `
+        <div class="card shadow-lg col mt-4  mx-auto">
+            <div class="card-header bg-success container-fluid">
+                  <p class="h4 text-dark">${clist[index]}</p>
+            </div>
+            <div class="card-body">
+                <img src="${item}" style="width:150px; height:150px" alt="error"/>
+                <br/><br/>
+                <a href="#" class="text-warning btn btn-success">Know about company!</a>
+                </div>
+            </div>
         </div>
-      <div class="card-body">
-          <p class="card-text">${data.regno.toUpperCase()}</p>
-          
-          <p class="card-text">${generateCompanyListHTML(data.companies,data.salaries,data.types)}</p>
-   
-          
-          
-      </div>
-  </div>
-`;
+      `;
+    
+            cardContainer.appendChild(card);
+    
+        });
+    }
 
 
-  function generateCompanyListHTML(companylist, salary, types) {
-      let html = "<ol>"; // Assuming a list structure for array items
 
-      companylist.forEach(function (item, index) {
-          var text = "LPA"
-          if (salary[index] == "NA") {
-              salary[index] = "Unknown"
-              text = ""
-          }
-
-
-          html += `<li>${item} : <b>${salary[index]} ${text} </b> (${types[index]})</li>`;
-
-      });
-
-
-      html += "</ol>";
-
-      return html;
-  }
-
-  // Append card to the container
-  //cardContainer.appendChild(card);
 }
+
+
+document.getElementById("deptheading").innerHTML = "Y20 Placements";
+
+
 
 
 function get(x, y) {
 
-  document.getElementById("cardContainer").innerHTML = ""
-  var firebaseConfig = {
-      apiKey: "AIzaSyBSd0FFY4jYumI731SXaZ1RpAEXsrp6fog",
-      authDomain: "bec-hostels-64d05.firebaseapp.com",
-      databaseURL: "https://bec-hostels-64d05-default-rtdb.firebaseio.com",
-      projectId: "bec-hostels-64d05",
-      storageBucket: "bec-hostels-64d05.appspot.com",
-      messagingSenderId: "174951857966",
-      appId: "1:174951857966:web:abe5f329cb70d6c01ac4bf",
-      measurementId: "G-9J50LVPRKQ"
-  };
+    document.getElementById("cardContainer").innerHTML = ""
+    var firebaseConfig = {
+        apiKey: "AIzaSyBSd0FFY4jYumI731SXaZ1RpAEXsrp6fog",
+        authDomain: "bec-hostels-64d05.firebaseapp.com",
+        databaseURL: "https://bec-hostels-64d05-default-rtdb.firebaseio.com",
+        projectId: "bec-hostels-64d05",
+        storageBucket: "bec-hostels-64d05.appspot.com",
+        messagingSenderId: "174951857966",
+        appId: "1:174951857966:web:abe5f329cb70d6c01ac4bf",
+        measurementId: "G-9J50LVPRKQ"
+    };
 
 
-  if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
-  }
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
 
-  // Get the Firestore database instance
-  var db = firebase.firestore();
-
-
-  // Reference to Firestore
-
-  // Retrieve data and render cards
-  db.collection(`/becstuoweb/navigation/placements/y20/a` + x)
-      .orderBy("regno", "asc")
-      .get()
-      .then(
-          (querySnapshot) => {
-              querySnapshot.forEach((doc) => {
-                  const data = doc.data();
-                  renderCard(data);
-              });
-          }
-      )
-      .catch((error) => {
-          console.log("Error getting documents: ", error);
-      });
-
-  try {
-      document.getElementById("deptheading").innerHTML = "";
-      document.getElementById("deptheading").innerHTML = "Y20 " + y + " Placements";
-  }
-  catch (error) {
-      document.getElementById("deptheading").innerHTML = " Y20 Placements";
-
-  }
-
-  // Function to render a card
-  function renderCard(data) {
+    // Get the Firestore database instance
+    var db = firebase.firestore();
 
 
-      const cardContainer = document.getElementById("cardContainer");
+    // Reference to Firestore
+
+    // Retrieve data and render cards
+    db.collection(`/becstuoweb/navigation/placements/y20/a` + x)
+        .orderBy("regno", "asc")
+        .get()
+        .then(
+            (querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    const data = doc.data();
+                    renderCard(data);
+                });
+            }
+        )
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+
+    try {
+        document.getElementById("deptheading").innerHTML = "";
+        document.getElementById("deptheading").innerHTML = "Y20 " + y + " Placements";
+    }
+    catch (error) {
+        document.getElementById("deptheading").innerHTML = " Y20 Placements";
+
+    }
+
+    // Function to render a card
+    function renderCard(data) {
+
+
+        const cardContainer = document.getElementById("cardContainer");
 
 
 
-      // Create card element
-      const card = document.createElement("div");
-      card.className = "col-md-4 mb-4";
+        // Create card element
+        const card = document.createElement("div");
+        card.className = "col-md-4 mb-4";
 
-      /*
-      <div class="card shadow-lg col-lg-3 col-md-12 col-sm-10  mx-auto" >
-              <div class="card-header bg-success container-fluid">
-                  <p class="h4 ">BEC Studo</p>
+        /*
+        <div class="card shadow-lg col-lg-3 col-md-12 col-sm-10  mx-auto" >
+                <div class="card-header bg-success container-fluid">
+                    <p class="h4 ">BEC Studo</p>
+                </div>
+                <img src="https://play-lh.googleusercontent.com/GB1_i_I46wgNtzwITnpPNMyU6EW_O7giINmLnIzRYJN6A9bdqxgbJJbRGIkCrKsIBng=w240-h480-rw" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Android Application</h5>
+                  <p class="card-text">For the students of Bapatla Engineering College</p>
+                  <a href="https://play.google.com/store/apps/details?id=com.iTKonnects.becfrag" class="btn btn-primary">View App in Play store</a>
+                </div>
               </div>
-              <img src="https://play-lh.googleusercontent.com/GB1_i_I46wgNtzwITnpPNMyU6EW_O7giINmLnIzRYJN6A9bdqxgbJJbRGIkCrKsIBng=w240-h480-rw" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Android Application</h5>
-                <p class="card-text">For the students of Bapatla Engineering College</p>
-                <a href="https://play.google.com/store/apps/details?id=com.iTKonnects.becfrag" class="btn btn-primary">View App in Play store</a>
-              </div>
-            </div>
+  
+              */
 
-            */
-
-      card.innerHTML = `
+        card.innerHTML = `
         <div class="card shadow-lg col mt-4  mx-auto">
               <div class="card-header bg-success container-fluid">
                   <p class="h4 text-white">${data.name}</p>
@@ -245,10 +171,10 @@ function get(x, y) {
                 <p class="card-text">${data.regno.toUpperCase()}</p>
                 
                 <p class="card-text">${generateCompanyListHTML(
-          data.companies,
-          data.salaries,
-          data.types
-      )}</p>
+            data.companies,
+            data.salaries,
+            data.types
+        )}</p>
          
                 
                 <!-- Add other fields as needed -->
@@ -257,29 +183,29 @@ function get(x, y) {
     `;
 
 
-      function generateCompanyListHTML(companylist, salary, types) {
-          let html = "<ol>"; // Assuming a list structure for array items
+        function generateCompanyListHTML(companylist, salary, types) {
+            let html = "<ol>"; // Assuming a list structure for array items
 
-          companylist.forEach(function (item, index) {
-              var text = "LPA"
-              if (salary[index] == "NA") {
-                  salary[index] = "Unknown"
-                  text = ""
-              }
-
-
-              html += `<li>${item} : <b>${salary[index]} ${text} </b> (${types[index]})</li>`;
-
-          });
+            companylist.forEach(function (item, index) {
+                var text = "LPA"
+                if (salary[index] == "NA") {
+                    salary[index] = "Unknown"
+                    text = ""
+                }
 
 
-          html += "</ol>";
+                html += `<li>${item} : <b>${salary[index]} ${text} </b> (${types[index]})</li>`;
 
-          return html;
-      }
+            });
 
-      // Append card to the container
-      cardContainer.appendChild(card);
-  }
+
+            html += "</ol>";
+
+            return html;
+        }
+
+        // Append card to the container
+        cardContainer.appendChild(card);
+    }
 
 }
--- js/plamaterials.js
+++ js/plamaterials.js
@@ -1,12 +1,78 @@
+function mainer() {
+
+  document.getElementById("deptheading").innerHTML = "Placement preparation materials";
+
+
+  var card = document.getElementById("cardContainer");
+  card.innerHTML = '';
+  var imageslist = [
+      "https://p92.com/binaries/content/gallery/p92website/technologies/htmlcssjs-overview.png",
+      "https://play-lh.googleusercontent.com/Sj_s77vDeBcy5lrjUIOJBtOOJTm8uQ15PBB03rmzgUTxqvvTFXl08VlX0xyq7bYjg0o",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjyHA0z64Zo0Ovz0X6MSIoyjqPYkUVHYFgoQ&usqp=CAU",
+      "https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124",
+      "https://cmrtc.ac.in/wp-content/uploads/2021/08/Placement_6-2021.jpg",
+      "https://miro.medium.com/v2/resize:fit:1400/0*P0HNukHYYJ7bZ7Qs.png"
+  ];
+
+
+  if (card) {
+      // Container HTML
+      var containerHTML = `
+      <div class="container" style="border: solid 2px black; margin-top:40px; width:500px; height:300px; padding-bottom: 10%; position: relative; overflow: hidden;">
+          <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="true">
+              <div class="carousel-indicators">`
+
+      imageslist.forEach(function (index) {
+          containerHTML += `<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="${index + 1}" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  `
+      })
+      containerHTML += `</div>
+              <div class="carousel-inner">`;
+
+      // Carousel Items HTML
+      var carouselItemsHTML = '';
+      imageslist.forEach(function (item, index) {
+          carouselItemsHTML += `
+          <div class="carousel-item ${index === 0 ? 'active' : ''}"  data-bs-interval="1000">
+              <img src="${item}" class="d-block" style="width:500px; height:300px; padding:10px" alt="...">
+          </div>`;
+      });
+
+      // Controls and closing tags
+      var closingHTML = `
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+              </button>
+          </div>
+      </div>`;
+
+      // Set the HTML content of the card element
+      card.innerHTML += containerHTML + carouselItemsHTML + closingHTML;
+  } else {
+      console.error("Container element not found.");
+  }
+
+
+}
+
+
+//outside mainer and not necessary i think
+
 var firebaseConfig = {
-    apiKey: "AIzaSyBSd0FFY4jYumI731SXaZ1RpAEXsrp6fog",
-    authDomain: "bec-hostels-64d05.firebaseapp.com",
-    databaseURL: "https://bec-hostels-64d05-default-rtdb.firebaseio.com",
-    projectId: "bec-hostels-64d05",
-    storageBucket: "bec-hostels-64d05.appspot.com",
-    messagingSenderId: "174951857966",
-    appId: "1:174951857966:web:abe5f329cb70d6c01ac4bf",
-    measurementId: "G-9J50LVPRKQ"
+  apiKey: "AIzaSyBSd0FFY4jYumI731SXaZ1RpAEXsrp6fog",
+  authDomain: "bec-hostels-64d05.firebaseapp.com",
+  databaseURL: "https://bec-hostels-64d05-default-rtdb.firebaseio.com",
+  projectId: "bec-hostels-64d05",
+  storageBucket: "bec-hostels-64d05.appspot.com",
+  messagingSenderId: "174951857966",
+  appId: "1:174951857966:web:abe5f329cb70d6c01ac4bf",
+  measurementId: "G-9J50LVPRKQ"
 };
 
 
@@ -16,59 +82,198 @@ firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
 
-
-// Retrieve documents from Firestore
-db.collection("/Documents/y20/pla").orderBy("title","asc").get().then(function(querySnapshot) {
-var documentsDiv = document.getElementById("documents");
-
-querySnapshot.forEach(function(doc) {
-  var documentData = doc.data();
-
-  var d1=document.createElement("div");
-  d1.className="card";
-
-  var d2=document.createElement("div");
-  d2.className="row no-gutters";
-
-  var d3=document.createElement("div");
-  d3.className="col-md-4";
-
-  var d4=document.createElement("div");
-  d4.className="col-md-8";
-
-  var d5=document.createElement("div");
-  d5.className="card-body";
-
-  var title=document.createElement("h5");
-  title.className="card-title";
-  title.textContent=documentData.title;
-
-  var des=document.createElement("p");
-  des.textContent=documentData.des;
-  des.className="card-text";
-
-
-  var link = document.createElement("a");
-  link.textContent = "Click to open file" 
-  link.href = documentData.link
-  link.target = "_blank"; // Opens the link in a new tab/window
+document.getElementById("deptheading").innerHTML = "";
 
 
 
-  
-  d2.appendChild(d3);
-  
+/*
 
-  d5.appendChild(title);
-  d5.appendChild(des);
-  d5.appendChild(link);
-  
+// Reference to Firestore
+var card = document.getElementById("cardContainer").innerHTML = "";
 
-  d4.appendChild(d5);
-  d2.appendChild(d4);
+// Retrieve data and render cards
+db.collection("/Documents/placementmaterials/" + x)
 
-  d1.appendChild(d2);
-  
-  documentsDiv.appendChild(d1);
-});
-});
+  .get()
+  .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          renderCard(data);
+      });
+  })
+  .catch((error) => {
+      console.log("Error getting documents: ", error);
+  });
+
+try {
+  document.getElementById("deptheading").innerHTML = "";
+  document.getElementById("deptheading").innerHTML = "Y20 " + y + " Placements";
+}
+catch (error) {
+  document.getElementById("deptheading").innerHTML = "";
+
+}
+
+// Function to render a card
+function renderCard(data) {
+  const cardContainer = document.getElementById("cardContainer");
+
+  // Create card element
+  const card = document.createElement("div");
+  card.className = "col-md-4 mb-4";
+
+  card.innerHTML = `
+  <div class="card shadow-lg col mt-4  mx-auto">
+        <div class="card-header bg-success container-fluid">
+            <p class="h4 text-warning">${data.name}</p>
+        </div>
+      <div class="card-body">
+          <p class="card-text">${data.regno.toUpperCase()}</p>
+          
+          <p class="card-text">${generateCompanyListHTML(data.companies, data.salaries, data.types)}</p>
+   
+          
+          
+      </div>
+  </div>
+`;
+
+
+  function generateCompanyListHTML(companylist, salary, types) {
+      let html = "<ol>"; // Assuming a list structure for array items
+
+      companylist.forEach(function (item, index) {
+          var text = "LPA"
+          if (salary[index] == "NA") {
+              salary[index] = "Unknown"
+              text = ""
+          }
+
+
+          html += `<li>${item} : <b>${salary[index]} ${text} </b> (${types[index]})</li>`;
+
+      });
+
+
+      html += "</ol>";
+
+      return html;
+  }
+
+  // Append card to the container
+  //cardContainer.appendChild(card);
+}
+
+*/
+
+function get(x, y) {
+
+  document.getElementById("cardContainer").innerHTML = ""
+  var firebaseConfig = {
+      apiKey: "AIzaSyBSd0FFY4jYumI731SXaZ1RpAEXsrp6fog",
+      authDomain: "bec-hostels-64d05.firebaseapp.com",
+      databaseURL: "https://bec-hostels-64d05-default-rtdb.firebaseio.com",
+      projectId: "bec-hostels-64d05",
+      storageBucket: "bec-hostels-64d05.appspot.com",
+      messagingSenderId: "174951857966",
+      appId: "1:174951857966:web:abe5f329cb70d6c01ac4bf",
+      measurementId: "G-9J50LVPRKQ"
+  };
+
+
+  if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+  }
+
+  // Get the Firestore database instance
+  var db = firebase.firestore();
+
+
+  // Reference to Firestore
+
+  // Retrieve data and render cards
+  db.collection("/Documents/placementmaterials/" + x)
+      .orderBy("title", "asc")
+      .get()
+      .then(
+          (querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                  const data = doc.data();
+                  renderCard(data);
+              });
+          }
+      )
+      .catch((error) => {
+          console.log("Error getting documents: ", error);
+      });
+
+  try {
+      document.getElementById("deptheading").innerHTML = "";
+      document.getElementById("deptheading").innerHTML = y;
+  }
+  catch (error) {
+      document.getElementById("deptheading").innerHTML = y;
+
+  }
+
+  // Function to render a card
+  function renderCard(data) {
+
+
+      const cardContainer = document.getElementById("cardContainer");
+
+
+
+      // Create card element
+      const card = document.createElement("div");
+      card.className = "col-md-4 mb-4";
+
+
+
+      card.innerHTML = `
+        <div class="card shadow-lg col mt-4  mx-auto">
+              <div class="card-header bg-primary container-fluid">
+                  <p class="h4 text-white">${data.title}</p>
+              </div>
+            <div class="card-body">
+
+                <p class="text-dark">${data.date}</p>
+                <p class="text-dark h5">${data.type}</p>
+                <p class="text-dark h6">${data.des}</p>  
+                
+                  <button class="btn btn-success text-white">
+                      <a class="card-text text-white" href=${data.link} target="_blank">View file</a>
+                  </button> 
+                  
+                
+            </div>
+        </div>
+    `;
+
+
+      function generateCompanyListHTML(companylist, salary, types) {
+          let html = "<ol>"; // Assuming a list structure for array items
+
+          companylist.forEach(function (item, index) {
+              var text = "LPA"
+              if (salary[index] == "NA") {
+                  salary[index] = "Unknown"
+                  text = ""
+              }
+
+
+              html += `<li>${item} : <b>${salary[index]} ${text} </b> (${types[index]})</li>`;
+
+          });
+
+
+          html += "</ol>";
+
+          return html;
+      }
+
+      // Append card to the container
+      cardContainer.appendChild(card);
+  }
+
+}
